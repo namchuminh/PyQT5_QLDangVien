b6f78405d738dc3e616043202241c083622661e9
Nam Chu Minh <chuminhnamma@gmail.com>
2023-09-18 11:19:32 +0700

    hoan thanh chuc nang nhap dang vien tu csv

--- src/DangVien.py
+++ src/DangVien.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QHeaderView, QMessageBox
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QMessageBox, QFileDialog
 from PyQt5.uic import loadUi
 from database.connect import conndb
 from datetime import date
+import pandas as pd
 
 class DangVien(QWidget):
 
@@ -19,7 +20,7 @@ class DangVien(QWidget):
         self.buttonSua.clicked.connect(self.update)
         self.buttonXoa.clicked.connect(self.delete)
         self.buttonTimKiem.clicked.connect(self.search)
-
+        self.buttonNhap.clicked.connect(self.importCSV)
 
         self.buttonSua.setDisabled(True)
         self.buttonXoa.setDisabled(True)
@@ -107,7 +108,7 @@ class DangVien(QWidget):
             self.tableData.setColumnCount(21)
 
         # Đặt tên cho các cột
-        self.tableData.setHorizontalHeaderLabels(['Mã ĐV', 'Số Thẻ', 'Họ Tên', 'Số CCCD', 'Ngày Sinh', 'Giới Tính', 'Dân Tộc', 'Đảng viên', 'Quê Quán', 'Thường Trú', 'Nơi Sinh', 'Nơi Ở', 'Ngày Vào Đảng', 'Ngày Chính Thức', 'Số ĐT', 'Email', 'Chi Bộ', 'Chức Vụ Đảng', 'Chức Vụ CQ', 'Trình Độ LLCT', 'Chuyên Môn'])
+        self.tableData.setHorizontalHeaderLabels(['Mã ĐV', 'Số Thẻ', 'Họ Tên', 'Số CCCD', 'Ngày Sinh', 'Giới Tính', 'Dân Tộc', 'Tôn Giáo', 'Quê Quán', 'Thường Trú', 'Nơi Sinh', 'Nơi Ở', 'Ngày Vào Đảng', 'Ngày Chính Thức', 'Số ĐT', 'Email', 'Chi Bộ', 'Chức Vụ Đảng', 'Chức Vụ CQ', 'Trình Độ LLCT', 'Chuyên Môn'])
 
         for row, rowData in enumerate(data):
             for col, value in enumerate(rowData):
@@ -328,7 +329,126 @@ class DangVien(QWidget):
         else:
             self.showDataTable(result)
 
+    def importCSV(self):
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(self, "Chọn file .csv", "", "CSV Files (*.csv)")
+        flag_insert = True
+        if file_path:
+            try:
+                # Đọc file .csv bằng pandas
+                df = pd.read_csv(file_path, encoding='utf-8')
+
+                for index, row in df.iterrows():
+                    result = self.getForeignKeyAfterImport(row['Dân Tộc'], row['Tôn Giáo'], row['Chi Bộ'], row['Chức Vụ Đảng'], row['Chức Vụ CQ'], row['Trình Độ LLCT'], row['Chuyên Môn'])
+                    if result[0] == False:
+                        self.messageBoxInfo("Thông Báo", result[1])
+                        flag_insert = False
+                        break
+
+                try:
+                    if flag_insert == True:
+                        for index, row in df.iterrows():
+                            result = self.getForeignKeyAfterImport(row['Dân Tộc'], row['Tôn Giáo'], row['Chi Bộ'], row['Chức Vụ Đảng'], row['Chức Vụ CQ'], row['Trình Độ LLCT'], row['Chuyên Môn'])
+                            self.insertDatabaseAfterImport(row['Mã ĐV'], row['Số Thẻ'], result[2], row['Họ Tên'], row['Giới Tính'], result[0], result[1], row['Ngày Sinh'], row['Nơi Sinh'], row['Quê Quán'], row['Thường Trú'], row['Nơi Ở'], result[4], result[3], row['Ngày Vào Đảng'], row['Ngày Chính Thức'], row['Số CCCD'], row['Số ĐT'], row['Email'], result[5], result[6])
+
+                    query = f"""
+                        SELECT dangvien.MaDangVien, dangvien.SoTheDang, dangvien.HoTen, dangvien.SoCCCD, dangvien.NgaySinh, dangvien.GioiTinh, dantoc.TenDanToc, tongiao.TenTonGiao,dangvien.QueQuan, dangvien.HoKhauThuongTru, dangvien.NoiSinh, dangvien.DiaChiHienTai, dangvien.NgayVaoDang, dangvien.NgayChinhThuc, dangvien.SoDienThoai, dangvien.Email, chibo.TenChiBo, chucvudang.ChucVuDang, chucvuchinhquyen.ChucVuChinhQuyen, lyluanchinhtri.TrinhDoChinhTri, trinhdo.ChuyenMon 
+                        FROM dangvien, chibo, chucvudang, chucvuchinhquyen, dantoc, lyluanchinhtri, tongiao, trinhdo 
+                        WHERE dangvien.MaChiBo = chibo.MaChiBo AND dangvien.MaDanToc = dantoc.MaDanToc AND dangvien.MaTonGiao = tongiao.MaTonGiao AND dangvien.MaChucVuChinhQuyen = chucvuchinhquyen.MaChucVu AND dangvien.MaChucVuDang = chucvudang.MaChucVu AND dangvien.MaTrinhDoLyLuanChinhTri = lyluanchinhtri.MaTrinhDo AND dangvien.MaTrinhDoChuyenMon = trinhdo.MaTrinhDo 
+                        ORDER BY dangvien.id DESC LIMIT {df.shape[0]};
+                    """
+                    result = self.db.queryResult(query)
+                    self.showDataTable(result)
+
+                    self.buttonLamMoi.setText('XEM TẤT CẢ')
+
+                    self.messageBoxInfo("Thông Báo", "Nhập thành công thông tin Đảng viên!")
+
+                except Exception as e:
+                    self.messageBoxInfo("Thông Báo", e)
+
+            except pd.errors.ParserError:
+                error_message = "File không đúng định dạng .csv"
+                QMessageBox.critical(self, "Lỗi", error_message)
+    
+    def insertDatabaseAfterImport(self, MaDangVien, SoTheDang, MaChiBo, HoTen, GioiTinh, MaDanToc, MaTonGiao, NgaySinh, NoiSinh, QueQuan, HoKhauThuongTru, DiaChiHienTai, MaChucVuChinhQuyen, MaChucVuDang, NgayVaoDang, NgayChinhThuc, SoCCCD, SoDienThoai, Email, MaTrinhDoLyLuanChinhTri, MaTrinhDoChuyenMon):
+        ngaySinh = datetime.strptime(NgaySinh, "%d/%m/%Y")
+        ngaySinh = ngaySinh.strftime("%Y-%m-%d")
+
+        ngayVaoDang = datetime.strptime(NgayVaoDang, "%d/%m/%Y")
+        ngayVaoDang = ngayVaoDang.strftime("%Y-%m-%d")
+
+        ngayChinhThuc = datetime.strptime(NgayChinhThuc, "%d/%m/%Y")
+        ngayChinhThuc = ngayChinhThuc.strftime("%Y-%m-%d")
+
+        query = f"""
+                    INSERT INTO `dangvien`(`MaDangVien`, `SoTheDang`, `MaChiBo`, `HoTen`, `GioiTinh`, `MaDanToc`, `MaTonGiao`, `NgaySinh`, `NoiSinh`, `QueQuan`, `HoKhauThuongTru`, `DiaChiHienTai`, `MaChucVuChinhQuyen`, `MaChucVuDang`, `NgayVaoDang`, `NgayChinhThuc`, `SoCCCD`, `SoDienThoai`, `Email`, `MaTrinhDoLyLuanChinhTri`, `MaTrinhDoChuyenMon`) 
+                    VALUES ('{MaDangVien}','{SoTheDang}','{MaChiBo}','{HoTen}','{GioiTinh}','{MaDanToc}','{MaTonGiao}','{ngaySinh}','{NoiSinh}','{QueQuan}','{HoKhauThuongTru}','{DiaChiHienTai}','{MaChucVuChinhQuyen}','{MaChucVuDang}','{ngayVaoDang}','{ngayChinhThuc}','{SoCCCD}','0{SoDienThoai}','{Email}','{MaTrinhDoLyLuanChinhTri}','{MaTrinhDoChuyenMon}')
+                """
+        self.db.queryExecute(query)
+
+    def getForeignKeyAfterImport(self, dantoc, tongiao, chibo, chucvudang, chucvuchinhquyen, trinhdoLLCT, chuyenmon):
+        madantoc, matongiao, machibo, machucvudang, machucvuchinhquyen, matrinhdoLLCT, machuyenmon = "", "", "", "", "", "", ""
+        
+        query = f"SELECT * FROM dantoc WHERE TenDanToc LIKE '%{dantoc}%'"
+        result = self.db.queryResult(query)
+        if(len(result) > 0):
+            madantoc = result[0][0]
+        else:
+            return [False, f"Dân tộc '{dantoc}' không tồn tại trong hệ thống!"]
+
+
+        query = f"SELECT * FROM tongiao WHERE TenTonGiao LIKE '%{tongiao}%'"
+        result = self.db.queryResult(query)
+        if(len(result) > 0):
+            matongiao = result[0][0]
+        else:
+            return [False, f"Tôn giáo '{tongiao}' không tồn tại trong hệ thống!"]
+        
+
+        query = f"SELECT * FROM chibo WHERE TenChiBo LIKE '%{chibo}%'"
+        result = self.db.queryResult(query)
+        if(len(result) > 0):
+            machibo = result[0][0]
+        else:
+            return [False, f"Chi bộ '{chibo}' không tồn tại trong hệ thống!"]
+        
+
+        query = f"SELECT * FROM chucvudang WHERE ChucVuDang LIKE '%{chucvudang}%'"
+        result = self.db.queryResult(query)
+        if(len(result) > 0):
+            machucvudang = result[0][0]
+        else:
+            return [False, f"Chức vụ Đảng '{chucvudang}' không tồn tại trong hệ thống!"]
+        
+
+        query = f"SELECT * FROM chucvuchinhquyen WHERE ChucVuChinhQuyen LIKE '%{chucvuchinhquyen}%'"
+        result = self.db.queryResult(query)
+        if(len(result) > 0):
+            machucvuchinhquyen = result[0][0]
+        else:
+            return [False, f"Chức vụ Chính quyền '{chucvuchinhquyen}' không tồn tại trong hệ thống!"]
+
+
+        query = f"SELECT * FROM lyluanchinhtri WHERE TrinhDoChinhTri LIKE '%{trinhdoLLCT}%'"
+        result = self.db.queryResult(query)
+        if(len(result) > 0):
+            matrinhdoLLCT = result[0][0]
+        else:
+            return [False, f"Trình độ LLCT '{trinhdoLLCT}' không tồn tại trong hệ thống!"]
+        
+
+        query = f"SELECT * FROM trinhdo WHERE ChuyenMon LIKE '%{chuyenmon}%'"
+        result = self.db.queryResult(query)
+        if(len(result) > 0):
+            machuyenmon = result[0][0]
+        else:
+            return [False, f"Trình độ chuyên môn '{chuyenmon}' không tồn tại trong hệ thống!"]
+        
+        return [madantoc, matongiao, machibo, machucvudang, machucvuchinhquyen, matrinhdoLLCT, machuyenmon]
+
     def resetInput(self):
+        self.buttonLamMoi.setText('LÀM MỚI')
         self.txtMaDangVien.setText("")
         self.txtSoTheDangVien.setText("")
         self.txtHoTen.setText("")
